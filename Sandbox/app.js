20fe693df89a483409bfd2842955db3faf8fc86f
Gustavo Caldas <lgustavorcaldas@gmail.com>
2022-08-03 15:58:57 -0300

    Tentando docker

--- Sandbox/app.js
+++ Sandbox/app.js
@@ -11,34 +11,41 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", async (req, res) => {
+  try {
     const { albumId } = req.query;
-    const photos = await getOrSetCache( `photos?albumId=${albumId}`, 
-        async () => {
-            const { data } = await axios.get( 
-                "https://jsonplaceholder.typicode.com/photos", { params: { albumId } }
-            );
-            return data;
-        }
+    const photos = await getOrSetCache(`photos?albumId=${albumId}`, 
+      async () => {
+        const { data } = await axios.get( 
+          "https://jsonplaceholder.typicode.com/photos",
+          { params: { albumId } }
+        );
+        return data;
+      }
     );
     res.json(photos);
+  } catch (error) {
+    console.error(error)
+  }
 });
 
 function getOrSetCache (key, cb){
-    return new Promise((resolve, reject) => {
-        redis.get(key, 
-            async (err, data) => {
-                if (err) return reject(err);
-                if (data != null ){
-                    console.log("Redis Hit!");
-                    return resolve(JSON.parse(data));
-                } 
-                console.log("Redis Miss!");
-                const newData = await cb();
-                redis.setex(key, 60, JSON.stringify(newData));
-                resolve(newData);
-            }
-        );
-    });
+  return new Promise(
+    (resolve, reject) => {
+      redis.get(key, 
+        async (err, data) => {
+          if (err) return reject(err);
+          if (data != null ){
+            console.log("Redis Hit!");
+            return resolve(JSON.parse(data));
+          } 
+          console.log("Redis Miss!");
+          const newData = await cb();
+          redis.setex(key, 60, JSON.stringify(newData));
+          resolve(newData);
+        }
+      );
+    }
+  );
 };
 
 app.listen(port, console.log("App listening to port: " + port));
