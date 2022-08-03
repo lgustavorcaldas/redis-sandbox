58d7b4898abb99ddaa1f3602f875852c0adcc23e
Gustavo Caldas <lgustavorcaldas@gmail.com>
2022-08-03 14:49:45 -0300

    endentação

--- Sandbox/app.js
+++ Sandbox/app.js
@@ -4,36 +4,40 @@ const axios = require("axios");
 const port = 8080;
 
 const Redis = require("ioredis");
-const redisClient = Redis.createClient();
+const redis = Redis.createClient();
 
 const cors = require("cors");
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", async (req, res) => {
-    const albumId = req.query.albumId;
-    const photos = await getOrSetCache( `photos?albumId=${albumId}`, async () => {
-        const { data } = await axios.get( 
-            "https://jsonplaceholder.typicode.com/photos", { params: { albumId } }
-        );
-        return data;
-    });
+    const { albumId } = req.query;
+    const photos = await getOrSetCache( `photos?albumId=${albumId}`, 
+        async () => {
+            const { data } = await axios.get( 
+                "https://jsonplaceholder.typicode.com/photos", { params: { albumId } }
+            );
+            return data;
+        }
+    );
     res.json(photos);
 });
 
 function getOrSetCache (key, cb){
     return new Promise((resolve, reject) => {
-        redisClient.get(key, async (error, data) => {
-            if (error) return reject(error);
-            if (data != null ){
-                console.log("Redis Hit!");
-                return resolve(JSON.parse(data));
-            } 
-            console.log("Redis Miss!");
-            const freshData = await cb();
-            redisClient.setex(key, 60, JSON.stringify(freshData));
-            resolve(freshData);
-        });
+        redis.get(key, 
+            async (err, data) => {
+                if (err) return reject(err);
+                if (data != null ){
+                    console.log("Redis Hit!");
+                    return resolve(JSON.parse(data));
+                } 
+                console.log("Redis Miss!");
+                const newData = await cb();
+                redis.setex(key, 60, JSON.stringify(newData));
+                resolve(newData);
+            }
+        );
     });
 };
 
