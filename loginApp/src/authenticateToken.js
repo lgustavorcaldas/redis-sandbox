2658014d664de36d7ea842e0ddaad500327eb39e
Gustavo Caldas <lgustavorcaldas@gmail.com>
2022-08-09 22:16:56 -0300

    refactory

--- loginApp/src/authenticateToken.js
+++ loginApp/src/authenticateToken.js
@@ -3,17 +3,31 @@ const jwt = require('jsonwebtoken');
 function authenticateToken(req, res, next) {
   try {
     const { accessToken } = req.cookies;
-    if (!accessToken) res.json({ auth: false, message: 'No valid token provided.' }).status(401);
-    jwt.verify( accessToken.split(' ')[0], process.env.ACCESS_TOKEN_SECRET,
-      ( err, username ) => {
-        if (err) return res.json({ auth: false, message: 'Failed to authenticate token.' }).status(500);
-        //  Adiciona o parametro username no objeto req!!! MUITO FODA
-        req.username = username;
-        next();
-      }
-    );
+    if (!accessToken){
+      console.error({ auth: false, message: 'No valid token provided.' });
+
+      return res.json({ auth: false, message: 'No valid token provided.' }).status(401);
+    } else {
+      jwt.verify( accessToken.split(' ')[0], process.env.ACCESS_TOKEN_SECRET,
+        ( error, username ) => {
+          if( error ){
+            console.error({ auth: false, message: 'Failed to authenticate token.' });
+
+            return res.status(500).json({ auth: false, message: 'Failed to authenticate token.', error });
+          } else {
+            //  Adiciona o parametro username no objeto req!!! MUITO FODA
+            req.username = username;
+            console.log({ message: `Trying to authenticate: ${username} was a success!` });
+
+            return next();
+          }
+        }
+      );
+    }
   } catch (error) {
-    console.log(error);
+    console.error({ error: 'Trying to authenticate' });
+
+    return res.status(400).json({ error: 'Trying to authenticate', error });
   }
 }
 
--- loginApp/src/db.js
+++ loginApp/src/db.js
@@ -2,8 +2,8 @@ const { Client } = require("pg");
 
 const client = new Client(process.env.FUNNY_URL);
 
-client.connect(function(err) {
-  if(err) return console.error('could not connect to postgres', err);
+client.connect( function(error) {
+  if(error) return console.error('could not connect to postgres', error);
 });
 
 module.exports = client;
